70f61a006fbbff0553c4a3dd6d315265c3edd766
Dmitry Malkov <dmitry.malkov@live.com>
2022-12-18 23:41:57 -0600

    Url and yank highlight

--- lua/dimusic/init.lua
+++ lua/dimusic/init.lua
@@ -3,6 +3,27 @@ vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSi
 vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
 vim.fn.sign_define("DiagnosticSignHint", { text = "", texthl = "DiagnosticSignHint" })
 
+local url_matcher =
+    "\\v\\c%(%(h?ttps?|ftp|file|ssh|git)://|[a-z]+[@][a-z]+[.][a-z]+:)%([&:#*@~%_\\-=?!+;/0-9a-z]+%(%([.;/?]|[.][.]+)[&:#*@~%_\\-=?!+/0-9a-z]+|:\\d+|,%(%(%(h?ttps?|ftp|file|ssh|git)://|[a-z]+[@][a-z]+[.][a-z]+:)@![0-9a-z]+))*|\\([&:#*@~%_\\-=?!+;/.0-9a-z]*\\)|\\[[&:#*@~%_\\-=?!+;/.0-9a-z]*\\]|\\{%([&:#*@~%_\\-=?!+;/.0-9a-z]*|\\{[&:#*@~%_\\-=?!+;/.0-9a-z]*})\\})+"
+
+vim.api.nvim_create_autocmd({ "VimEnter", "FileType", "BufEnter", "WinEnter" }, {
+    desc = "URL Highlighting",
+    group = vim.api.nvim_create_augroup("highlighturl", { clear = true }),
+    pattern = "*",
+    callback = function()
+        vim.fn.matchadd("HighlightURL", url_matcher, 15)
+    end,
+})
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+    desc = "Highlight yanked text",
+    group = vim.api.nvim_create_augroup("highlightyank", { clear = true }),
+    pattern = "*",
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+})
+
 require("dimusic.set")
 require("dimusic.remap")
 require("dimusic.packer")
