74312ff7bc3e8ca2098c4fd7c0685ce960e132e7
Dmitry Malkov <targtn@gmail.com>
2022-12-20 21:29:05 -0600

    disable undofile

--- lua/dimusic/set.lua
+++ lua/dimusic/set.lua
@@ -18,8 +18,9 @@ vim.opt.wrap = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/vim/undodir"
-vim.opt.undofile = true
+-- vim.opt.undodir = os.getenv("HOME") .. "/vim/undodir"
+-- vim.opt.undofile = true
+vim.opt.undolevels = 100
 
 --vim.opt.hlsearch = false
 vim.opt.incsearch = true
