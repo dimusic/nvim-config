c5b541e3f78cfa2351cf20e415f6316718cf302e
Dmitry Malkov <dmitry.malkov@live.com>
2022-12-25 17:18:09 -0600

    Replace neo-tree with nvim-tree

---
+++ after/plugin/nvim-tree.lua
@@ -0,0 +1,65 @@
+require("nvim-tree").setup({
+    sort_by = "case_sensitive",
+    view = {
+        adaptive_size = true,
+        mappings = {
+            -- list = {
+            --     { key = "u", action = "dir_up" },
+            -- },
+        },
+    },
+    renderer = {
+        -- group_empty = true,
+    },
+    filters = {
+        -- dotfiles = true,
+    },
+})
+vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<cr>", { desc = "File Tree Toggle" })
+vim.keymap.set("n", "<leader>o", "<cmd>NvimTreeFindFile<cr>", { desc = "Reveal File" })
+
+-- vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+--
+-- local neoTree = require("neo-tree")
+--
+-- -- File Tree
+-- vim.keymap.set("n", "<leader>e", "<cmd>NeoTreeFocusToggle<cr>", { desc = "File Tree Toggle" })
+-- vim.keymap.set("n", "<leader>o", function()
+--     neoTree.reveal_current_file()
+-- end, { desc = "Reveal File" })
+--
+-- require("neo-tree").setup({
+--     -- enable_diagnostics = false,
+--
+--     window = {
+--         width = 25,
+--     },
+--
+--     default_component_configs = {
+--         modified = {
+--             -- symbol = "[+]",
+--
+--             highlight = "NeoTreeModified",
+--         },
+--
+--         git_status = {
+--             symbols = {
+--                 -- Change type
+--                 -- added = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
+--                 -- modified = "🌝", -- or "", but this is redundant info if you use git_status_colors on the name
+--                 -- deleted = "✖", -- this can only be used in the git_status source
+--                 -- renamed = "", -- this can only be used in the git_status source
+--                 -- -- Status type
+--                 -- untracked = "",
+--                 -- ignored = "",
+--                 -- unstaged = "",
+--                 -- staged = "",
+--                 -- conflict = "",
+--             },
+--         },
+--     },
+--
+--     buffers = {
+--         follow_current_file = false,
+--     },
+-- })
