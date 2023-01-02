4c6603fb3ebf299f722ff7505d4faaca4fa79982
Dmitry Malkov <targtn@gmail.com>
2023-01-02 17:16:21 -0600

    Update cmp mappings for copilot

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -35,6 +35,13 @@ lsp.configure("sumneko_lua", {
 })
 
 local cmp = require("cmp")
+local has_words_before = function()
+    if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
+        return false
+    end
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
+end
 
 lsp.setup_nvim_cmp({
     sources = cmp.config.sources({
@@ -71,27 +78,21 @@ lsp.setup_nvim_cmp({
             return vim_item
         end,
     },
-    --mapping = cmp_mappings
 
-    -- sorting = {
-    --     priority_weight = 2,
-    --     comparators = {
-    --         require("copilot_cmp.comparators").prioritize,
-    --         require("copilot_cmp.comparators").score,
-    --
-    --         -- Below is the default comparitor list and order for nvim-cmp
-    --         cmp.config.compare.offset,
-    --         -- cmp.config.compare.scopes, --this is commented in nvim-cmp too
-    --         cmp.config.compare.exact,
-    --         cmp.config.compare.score,
-    --         cmp.config.compare.recently_used,
-    --         cmp.config.compare.locality,
-    --         cmp.config.compare.kind,
-    --         cmp.config.compare.sort_text,
-    --         cmp.config.compare.length,
-    --         cmp.config.compare.order,
-    --     },
-    -- },
+    mapping = lsp.defaults.cmp_mappings({
+        ["<CR>"] = cmp.mapping.confirm({
+            -- this is the important line
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = false,
+        }),
+        ["<Tab>"] = vim.schedule_wrap(function(fallback)
+            if cmp.visible() and has_words_before() then
+                cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+            else
+                fallback()
+            end
+        end),
+    }),
 })
 
 -- vim.cmd([[
