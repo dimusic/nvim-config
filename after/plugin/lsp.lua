f8838fb57b1b1bb51af4bb821e466fc2ebe8f8c6
Dmitry Malkov <dmitry.malkov@live.com>
2023-01-14 22:42:27 -0600

    Use table.unpack only when unpack is not available

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -34,10 +34,14 @@ lsp.configure("sumneko_lua", {
 
 local cmp = require("cmp")
 local has_words_before = function()
+    if not unpack then
+        unpack = table.unpack
+    end
+
     if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
         return false
     end
-    local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
     return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
 end
 
