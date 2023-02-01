e2e73f882b1a91af47a98423f4406e41b34c7274
Dmitry Malkov <targtn@gmail.com>
2023-01-31 21:49:09 -0600

    Remap ctrl-c

--- lua/dimusic/remap.lua
+++ lua/dimusic/remap.lua
@@ -25,3 +25,5 @@ vim.keymap.set("n", "<C-H>", "<C-W>h")
 vim.keymap.set("n", "<C-J>", "<C-W>j")
 vim.keymap.set("n", "<C-K>", "<C-W>k")
 vim.keymap.set("n", "<C-L>", "<C-W>l")
+
+vim.keymap.set("n", "<C-c>", "<Esc>")
