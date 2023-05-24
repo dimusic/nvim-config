a62ce51df807e98990698a90fa8a0693862f9bf5
Dmitry Malkov <targtn@gmail.com>
2023-05-23 21:10:39 -0600

    Fix trouble bindings

--- after/plugin/trouble.lua
+++ after/plugin/trouble.lua
@@ -8,7 +8,7 @@ vim.api.nvim_set_keymap(
 vim.api.nvim_set_keymap(
     "n",
     "<leader>xd",
-    "<cmd>TroubleToggle workspace_diagnostics<cr>",
+    "<cmd>TroubleToggle document_diagnostics<cr>",
     { noremap = true, silent = true }
 )
 vim.api.nvim_set_keymap("n", "gR", "<cmd>TroubleToggle lsp_references<cr>", { noremap = true, silent = true })
