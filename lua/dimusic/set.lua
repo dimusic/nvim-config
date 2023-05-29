83b9e7e34f5c401b8d6cc73973d1ff82d5427c26
Dmitry Malkov <targtn@gmail.com>
2023-05-28 22:07:19 -0600

    Support wgsl files

--- lua/dimusic/set.lua
+++ lua/dimusic/set.lua
@@ -80,3 +80,8 @@ autocmd FocusGained,BufEnter,CursorHold,CursorHoldI * if mode() != 'c' | checkti
 autocmd FileChangedShellPost *
 \ echohl WarningMsg | echo "File changed on disk. Buffer reloaded." | echohl None
 ]])
+
+vim.cmd([[
+autocmd BufNewFile,BufRead *.wgsl set filetype=wgsl
+]])
+
