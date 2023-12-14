94e9fb33aff69dd7410e1811c2bff82edc3da241
Dmitry Malkov <targtn@gmail.com>
2023-12-13 22:07:53 -0600

    Remove snippets

--- lua/dimusic/lazy.lua
+++ lua/dimusic/lazy.lua
@@ -79,13 +79,8 @@ require("lazy").setup({
             { "hrsh7th/nvim-cmp" },
             { "hrsh7th/cmp-buffer" },
             { "hrsh7th/cmp-path" },
-            { "saadparwaiz1/cmp_luasnip" },
             { "hrsh7th/cmp-nvim-lsp" },
             { "hrsh7th/cmp-nvim-lua" },
-
-            -- Snippets
-            { "L3MON4D3/LuaSnip" },
-            { "rafamadriz/friendly-snippets" },
         },
     },
 
