9e3fcfbc1f0c57d81d4c2bab22947ead4fc12e36
Dmitry Malkov <targtn@gmail.com>
2024-09-04 20:43:25 -0600

    Remove neoscroll

--- after/plugin/neoscroll.lua
+++ after/plugin/neoscroll.lua
@@ -1,11 +1,11 @@
-require("neoscroll").setup({
-    performance_mode = false,
-    mappings = { "<C-u>", "<C-d>", "<C-f>", "<C-b>", "zz", "zt", "zb" },
-})
-
-local t = {}
-t["<C-u>"] = { "scroll", { "-vim.wo.scroll", "true", "100" } }
-t["<C-d>"] = { "scroll", { "vim.wo.scroll", "true", "100" } }
-t["zz"] = { "zz", { "100" } }
-
-require("neoscroll.config").set_mappings(t)
+-- require("neoscroll").setup({
+--     performance_mode = false,
+--     mappings = { "<C-u>", "<C-d>", "<C-f>", "<C-b>", "zz", "zt", "zb" },
+-- })
+--
+-- local t = {}
+-- t["<C-u>"] = { "scroll", { "-vim.wo.scroll", "true", "100" } }
+-- t["<C-d>"] = { "scroll", { "vim.wo.scroll", "true", "100" } }
+-- t["zz"] = { "zz", { "100" } }
+--
+-- require("neoscroll.config").set_mappings(t)
