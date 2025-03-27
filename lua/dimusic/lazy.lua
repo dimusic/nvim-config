a32b3f8e74af985a826dad43725d0c0b38518be0
Dmitry Malkov <targtn@gmail.com>
2025-03-26 22:07:23 -0600

    Use make to build telescope-fzf-native

--- lua/dimusic/lazy.lua
+++ lua/dimusic/lazy.lua
@@ -20,7 +20,7 @@ require("lazy").setup({
 
     {
         "nvim-telescope/telescope-fzf-native.nvim",
-        build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+        build = "make",
     },
 
     { "smartpde/telescope-recent-files" },
